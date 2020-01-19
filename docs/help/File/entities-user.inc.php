ec4fda2b51009f957ad24bd706727a568e61abef
Gary Dean <garydean@okusi.id>
2020-01-19 15:50:00 +0700

    new help files

---
+++ docs/help/File/entities-user.inc.php
@@ -0,0 +1,12 @@
+#-----File--entities-user.inc.php
+      File: entities-user.inc.php
+      Desc: If a file with the name of entities-user.inc.sh exists in the
+          : Entities directory, is is automatically included at the
+          : end of the Entities script.
+          : By default, entities-user.inc.sh is symlinked to /dev/null.
+          : User should change this to point at their own bash script.
+          : User should not store their include script in the Entities directory.
+          : This file can be used to over-ride Entities defaults and set to
+          : User's commonly used defaults for global variables and functions,
+          : without having to change code in the core Entities script.
+#-----File--entities-user.inc.php---------------------------------------------
