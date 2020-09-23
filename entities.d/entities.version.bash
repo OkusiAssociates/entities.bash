7ee5150914153e2b7cf40f42d549674a422c5307
Gary Dean <garydean@okusi.id>
2020-09-23 19:35:09 +0700

    build [0.95.420.460.13]

--- entities.d/entities.version.bash
+++ entities.d/entities.version.bash
@@ -8,8 +8,8 @@
 #X          :   minorver  0
 #X          :   420       constant
 #X          :   day0      days since 2019-06-21 (460)
-#X          :   build     this is build #11 on day 460
-#X          : This is entities.bash version 0.95.420.460.11
+#X          :   build     this is build #13 on day 460
+#X          : This is entities.bash version 0.95.420.460.13
 declare -xg _ent_VERSION
-_ent_VERSION='0.95.420.460.11'
+_ent_VERSION='0.95.420.460.13'
 #fin
