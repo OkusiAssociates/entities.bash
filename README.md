44b3f1ea9ddacbab47791d799ec6b451fe6317ed
Gary Dean <garydean@okusi.id>
2020-09-27 17:46:52 +0700

    build [0.97.420.464.8]

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-##### Version [0.97.420.464.7]
+##### Version [0.97.420.464.8]
 ###### Version [0.97.420.462.10]
 # `entities.bash` Environment/Function Library
 
--- entities.d/sys/hr2int.bash
+++ entities.d/sys/hr2int.bash
@@ -7,18 +7,24 @@
 #X Example : hr2int 34M
 #X				 : hr2int 34m
 #X				 : hr2int 34000000
-declare -Aix _ent_HRint=( [b]=1 [k]=1024 [m]=1024000 [g]=1024000000 [t]=1024000000000 [p]=1024000000000000 \
+declare -Aixg _ent_HRint=( [b]=1 [k]=1024 [m]=1024000 [g]=1024000000 [t]=1024000000000 [p]=1024000000000000 \
 										 [B]=1 [K]=1000 [M]=1000000 [G]=1000000000 [T]=1000000000000 [P]=1000000000000000 )
 hr2int() {
-	local num='' h=''
+	local num='' h='' fmt=si
 	while (($#)); do
 		num=${1:-0} 
  		h=${num: -1}
-		if [[ 'bBkKmMgGtTpP' == *"$h"* ]]; then
-			echo $(( ${_ent_HRint[$h]} * ${num:0:-1})) || return 1
+		if [[ ${h:-} =~ ^[-+]?[0-9.]+$ ]]; then 
+			fmt=si
 		else
-			echo $(( num )) || return 1
+			local LC_ALL=C
+			if [[ "$h" > 'a' ]]; then 
+				fmt=iec
+			else 
+				fmt=si		
+			fi
 		fi
+		numfmt --from="$fmt" "${num^^}" || return 1
 		shift 1
 	done
 	return 0
@@ -26,22 +32,24 @@ hr2int() {
 declare -fx hr2int
 
 #X Function: int2hr 
-#X Synopsis: int2hr number hrchar [number hrchar]...
-#X				 : number is any integer.
-#X				 : hrchar is one of {b|B|k|K|m|M|g|G|t|T|p|P}
-#X Example : int2hr 34393827 m
-#X         : int2hr 10382 K 872929292929 g
+#X Desc    : Convert integer to human-readable string, using SI (base 1000)
+#X         : or IEC (1024) for conversion.
+#X Synopsis: int2hr number [si|iec] [number [si|iec]]...
+#X				 :   number   is any integer.
+#X				 :   si|iec   number format (si: 1k=1000, iec: 1K=1024)
+#X         :            Default format is 'si'.
+#X Example : int2hr 1000 si 1000 iec 1024 si 1024 iec
+#X         : int2hr 10382 iec 872929292929 si
 int2hr() {
-	local -i num=0
-	local h=''
+	local -i num 
+	local fmt hr
 	while (($#)); do
-		num=$(( ${1:-0} )) || return 1 
- 		h=${2:-b}
-		if [[ 'bBkKmMgGtTpP' == *"$h"* ]]; then
-			echo "$((num / ${_ent_HRint[$h]}))${h}" || return 1
-		else
-			echo >&2 "Invalid hr code [$h]"; return 1
-		fi
+		num=$(( ${1:-0} )) || { echo >&2 "Invalid number [$num]"; return 1; }
+ 		fmt=${2:-si}
+		fmt=${fmt,,}
+		hr=$(numfmt --to="$fmt" "$num") || { echo >&2 "Invalid hr code [$h]"; return 1; }
+		[[ $fmt == 'iec' ]] && hr="${hr,,}"
+		echo "$hr"
 		shift 1
 		(($#)) && shift 1
 	done
@@ -49,5 +57,3 @@ int2hr() {
 }
 declare -fx int2hr
 #fin
-
- 
