0420bc26df2100e5a532d0332ca0e727d22a28a1
Gary Dean <garydean@okusi.id>
2020-11-07 17:13:34 +0700

    build [0.98.420.505.1]

--- e.d/entities.version.bash
+++ e.d/entities.version.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #X Global : _ent_VERSION
-#X Version: 0.98.420.504.3
+#X Version: 0.98.420.505.1
 #X Desc   : Return version/build of this entities.bash.
 #X        : Returns string in the form:
 #X        :   majorver.minorver.420.day0.build
@@ -8,8 +8,8 @@
 #X        :   majorver  1
 #X        :   minorver  0
 #X        :   420       Constant
-#X        :   day0      Days since 2019-06-21 (504)
-#X        :   build     This is build #3 on day 504
+#X        :   day0      Days since 2019-06-21 (505)
+#X        :   build     This is build #1 on day 505
 declare -xg _ent_VERSION
-_ent_VERSION='0.98.420.504.3'
+_ent_VERSION='0.98.420.505.1'
 #fin
